bd4937ad22d1241815ea24a0e7fa3de1e0a5cccf
Antony <antonyjoel92@gmail.com>
2025-10-11 03:58:19 +0530

    Initial commit

---
+++ Git.java
@@ -0,0 +1,9 @@
+package gitpractice;
+
+public class Git {
+	    public static void main(String[] args) {
+	        System.out.println("Hello, Git and GitHub from Selenium Java Project!");
+	    }
+	}
+
+
