2f355e84d75108ef015bbf06c81c07028e5663e2
Antony <antonyjoel92@gmail.com>
2025-10-11 04:06:39 +0530

    Updated message to Version 2

--- Git.java
+++ Git.java
@@ -3,6 +3,7 @@ package gitpractice;
 public class Git {
 	    public static void main(String[] args) {
 	        System.out.println("Hello, Git and GitHub from Selenium Java Project!");
+	        System.out.println("Version 2: Git tracking works!");
 	    }
 	}
 
